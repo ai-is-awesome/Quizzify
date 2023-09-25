38558dad5525d38e2da922d35809f00f44bcdcc0
ai-is-awesome <irock0093@gmail.com>
2023-09-26 03:59:44 +0530

    added except statement in connecToDb

--- backend/db.js
+++ backend/db.js
@@ -2,9 +2,13 @@ import "dotenv/config";
 
 import mongoose from "mongoose";
 const connectToDb = async () => {
-  mongoose.connect(process.env.MONGO_DB_URL).then(() => {
-    console.log("Connected to db");
-  });
+  try {
+    mongoose.connect(process.env.MONGO_DB_URL).then(() => {
+      console.log("Connected to db");
+    });
+  } catch (e) {
+    console.log("Unable to connect to the DB");
+  }
 };
 
 export default connectToDb;
