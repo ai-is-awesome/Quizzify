60d3e6ff5a5fa131b32dabf6172ce5eae5d2e0df
ai-is-awesome <irock0093@gmail.com>
2023-09-26 04:07:45 +0530

    added try catchh block to prevent crashing server

--- backend/db.js
+++ backend/db.js
@@ -3,11 +3,10 @@ import "dotenv/config";
 import mongoose from "mongoose";
 const connectToDb = async () => {
   try {
-    mongoose.connect(process.env.MONGO_DB_URL).then(() => {
-      console.log("Connected to db");
-    });
+    await mongoose.connect(process.env.MONGO_DB_URL);
+    console.log("DB Connected");
   } catch (e) {
-    console.log("Unable to connect to the DB");
+    console.error("Unable to connect to the DB");
   }
 };
 
